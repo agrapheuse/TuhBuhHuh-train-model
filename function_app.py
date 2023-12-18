a27bd35a2e580ffa4e5f2a1af9c4f3c399c9dcda
Noah Diderich <noah.diderich@student.kdg.be>
2023-12-18 17:12:30 +0100

    creating models for each squares + documentation (README)

--- function_app.py
+++ function_app.py
@@ -12,51 +12,117 @@ from keras.callbacks import EarlyStopping
 import numpy as np
 import pickle
 
-
 app = func.FunctionApp()
 
 @app.schedule(schedule="0 0 * * 0", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def trainModel(myTimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function executed.')
+
+    # Connect to our blob storage
     connection_string = "DefaultEndpointsProtocol=https;AccountName=datalaketuhbehhuh;AccountKey=C2te9RgBRHhIH8u3tydAsn9wNd4umdD2axq1ZdcfKh7CZRpL04+D4H6QinE/gckMTUA/dFj1kFpd+ASt4+/8ZA==;EndpointSuffix=core.windows.net"
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # Get all the folders in the csv container
     folders = list_folders(blob_service_client, "csv")
-    merged = create_model(blob_service_client, list(folders)[0])
-    merged_on_time_df = merge_on_time(merged)
-    model = create_model_from_df(merged_on_time_df)
-    delete_previous_model()
-    store_model(model)
+
+    # loop through all the folders (all the squares in our grid)
+    for f in folders:
+        # Get the merged.csv file for the folder (contains all the data for the square)
+        merged_df = get_merged_df(blob_service_client, f)
+
+        """ 
+            It is supposed to take the latest data but that wouldnt be enough so we have a temporary
+            function that takes the data from 8:50 to 17:20 on 7th December 2023 (a time interval where we
+            have enough data for each square)
+
+            As soon as we have the function apps and the projects deployed and running continuously, we can
+            change this function to take the latest data.
+        """
+        # latest_data_df = get_latest(merged_df)
+        latest_data_df = temp_get_latest(merged_df)
+
+        # If there is no recent data, skip the square
+        if latest_data_df is None:
+            logging.info(f"Folder {f} has no recent data.")
+            continue
+        else:
+            print(latest_data_df.describe())
+
+        # Create the model unique to the square
+        model = create_model_from_df(latest_data_df)
+
+        # If there was an error in creating the model, skip the square
+        if model is None:
+            logging.info(f"No model created for folder {f}.")
+            continue
+
+        url_parts = f.split("/")
+
+        # Find the index of "history" and remove it
+        if "history" in url_parts:
+            url_parts.remove("history")
+        result_url = "/".join(url_parts)
+
+        # Delete the previous model and store the new one
+        delete_previous_model(blob_service_client, result_url)
+        store_model(blob_service_client, model, result_url)
+        logging.info(f"Model for folder {result_url} trained and stored.")
 
 def list_folders(blob_service_client, container_name):
+    # Connect to the container and extract a list of all the blobs
     container_client = blob_service_client.get_container_client(container_name)
     blob_list = container_client.list_blobs(name_starts_with="history/")
 
     folder_set = set()
 
+    # Extract the folder names from the blob names and add them to a set of all the folder names
     for blob in blob_list:
         blob_name = blob.name
-        # Extract folder name
         folder_name = "/".join(blob_name.split('/')[:-1])
         folder_set.add(folder_name)
 
     return folder_set
 
-def create_model(blob_service_client, folder):
-    container_client = blob_service_client.get_container_client("csv")
-    blob_list = container_client.list_blobs(name_starts_with=folder)
-    merged_df = pd.DataFrame(columns=["squareUUID", "timestamp", "BIKE", "CAR", "HEAVY", "HUMIDITY", "PEDESTRIAN", "PM10", "PM25", "TEMPERATURE"])
-    for blob in blob_list:
-        df = download_blob_to_file(blob_service_client, "csv", blob.name)
-        merged_df = merge_two_df(merged_df, df)
-    merged_df = merged_df.drop(columns="squareUUID", axis=1)
-    return merged_df
+# Download the merged.csv file for the square into a DataFrame
+def get_merged_df(blob_service_client, folder):
+    df = download_blob_to_file(blob_service_client, "csv", f"{folder}/merged.csv")
+    return df
 
-def merge_two_df(df1, df2):
-    appended_df = pd.concat([df1, df2], ignore_index=True)
-    return appended_df
+def get_latest(df):
+    # Drop columns with no data
+    columns_to_drop = []
+    for i in df.columns:
+        if df[i].count() == 0:
+            columns_to_drop.append(i)
 
-def download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name):
+    df = df.drop(columns=columns_to_drop)
+
+    # goes through the tail of the datafrale 10 rows at a time and checks if the average
+    # number of non-null values is less than 75% of the number of rows. if it, that
+    # means that there is not enough data for that square if we take more data so we return the dataframe
+    df_without_timestamp = df.drop(columns='timestamp')
+    for i in range(10, df.shape[0], 10):
+        temp_df = df_without_timestamp.iloc[-i:]
+        count_avg = temp_df.count().mean()
+        if count_avg < i * 0.75:
+            return temp_df
+
+def temp_get_latest(df):
+    # Takes the data from 8:50 to 17:20 on 7th December 2023 (a time interval where we
+    # have enough data for each square)
+    start_timestamp = '2023-12-07 08:50:00'
+    end_timestamp = '2023-12-07 17:20:00'
+    filtered_df = df[(df['timestamp'] >= start_timestamp) & (df['timestamp'] <= end_timestamp)]
+
+    threshold = 0.5  # Set the threshold percentage
+    nan_percentage = filtered_df.isna().mean()
+    columns_with_high_nan = nan_percentage[nan_percentage > threshold].index.tolist()
+    # Drop the column if it has more than 50% NaN values
+    filtered_df = filtered_df.drop(columns=columns_with_high_nan)
+    return filtered_df
+
+def download_blob_to_file(blob_service_client, container_name, blob_name):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     blob_data = blob_client.download_blob()
 
@@ -64,40 +130,24 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
     df = pd.read_csv(csv_file)
     return df
 
-def merge_on_time(df):
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df.set_index('timestamp', inplace=True)
-
-    # Resample the data to 5-minute intervals and calculate the mean
-    resampled_data = df.resample('5T').mean()
-
-    # Reset the index to make 'timestamp' a regular column again
-    resampled_data.reset_index(inplace=True)
-    for col in resampled_data.columns[1:]:
-        resampled_data[col] = resampled_data[col].round(1)
-    return resampled_data
-
 def create_model_from_df(df):
-    start_timestamp = '2023-12-07 08:50:00'
-    end_timestamp = '2023-12-07 17:20:00'
-    filtered_df = df[(df['timestamp'] >= start_timestamp) & (df['timestamp'] <= end_timestamp)]
-    threshold = 0.5  # Set the threshold percentage
-    nan_percentage = filtered_df.isna().mean()
-    columns_with_high_nan = nan_percentage[nan_percentage > threshold].index.tolist()
-    filtered_df = filtered_df.drop(columns=columns_with_high_nan)
+    # Drop the timestamp column
+    df = df.drop(columns='timestamp')
 
-    for col in filtered_df.columns[1:]:
-        filtered_df[col] = filtered_df[col].interpolate(method='linear', limit_direction='both')
+    # If there are no columns or rows, return None
+    if df.shape[1] == 0 or df.shape[0] == 0:
+        logging.info(f"Folder has no data.")
+        return None
 
-    filtered_df = filtered_df.drop(['timestamp'], axis=1)
-
+    # Interpolate the missing values
+    df_interpolated_linear = df.interpolate(method='linear')
+    
     # Convert the DataFrame to a NumPy array
-    data_array = filtered_df.values
+    data_array = df_interpolated_linear.values
 
     # Normalize the data
     scaler = StandardScaler()
     data_array_scaled = scaler.fit_transform(data_array)
-    print(data_array_scaled.shape)
 
     # Define window size and create sequences
     window_size = 36  # You can adjust this based on your data
@@ -133,19 +183,17 @@ def create_model_from_df(df):
     print(f'Model Test Loss: {loss}')
     return model
 
-def delete_previous_model():
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=datalaketuhbehhuh;AccountKey=C2te9RgBRHhIH8u3tydAsn9wNd4umdD2axq1ZdcfKh7CZRpL04+D4H6QinE/gckMTUA/dFj1kFpd+ASt4+/8ZA==;EndpointSuffix=core.windows.net"
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container="model", blob=f"model.pkl")
+def delete_previous_model(blob_service_client, folder):
+    # Delete the previous model in the specified folder
+    blob_client = blob_service_client.get_blob_client(container="model", blob=f"{folder}/model.pkl")
     blob_client.delete_blob()
     logging.info('Previous model deleted.')
 
-def store_model(model):
+def store_model(blob_service_client, model, folder):
+    # Store the new model in the specified folder as a pickle file
     model_pickle_string = pickle.dumps(model)
 
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=datalaketuhbehhuh;AccountKey=C2te9RgBRHhIH8u3tydAsn9wNd4umdD2axq1ZdcfKh7CZRpL04+D4H6QinE/gckMTUA/dFj1kFpd+ASt4+/8ZA==;EndpointSuffix=core.windows.net"
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container="model", blob=f"model.pkl")
+    blob_client = blob_service_client.get_blob_client(container="model", blob=f"{folder}/model.pkl")
     blob_client.upload_blob(model_pickle_string, blob_type="BlockBlob")
 
     logging.info('Model stored in blob storage.')
